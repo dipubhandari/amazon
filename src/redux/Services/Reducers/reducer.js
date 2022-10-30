90ea33c1369c6b76e955ae0276115ccadf39baf4
dipubhandari <dipendrabhandari123456@gmail.com>
2022-10-30 11:03:43 +0530

    add to cart paxi

--- src/redux/Services/Reducers/reducer.js
+++ src/redux/Services/Reducers/reducer.js
@@ -1,18 +1,38 @@
-// import 
 const InitialState = {
   cartData: [],
 }
+
 export default function cartReducer(state = InitialState, action) {
-  console.log(state.cartData.filter((item)=>{
-return item == 0
-  }))
+
 
   switch (action.type) {
-    case 'ADD_TO_CART': return {
-      ...state,
-     
-      cartData: [...state.cartData, action.data]
-    }
+
+    case 'ADD_TO_CART':
+      let exist = true
+      state.cartData.map((data) => {
+        if (action.data['id'] == data['id']) {
+           exist = false
+        }
+      }
+      )
+      if (exist) {
+        return {
+
+          ...state,
+          cartData: [...state.cartData, action.data]
+        }
+      }
+      else {
+       
+      }
+
+
+
+
+
+    case 'REMOVE_TO_CART': state.cartData.filter((removedCart) => {
+      return { cartData: [removedCart != action.data] }
+    })
     default:
 
       return state
