6efd2af87ea04b0597510b7140a378c18459effe
dipubhandari <dipendrabhandari123456@gmail.com>
2025-07-25 05:49:30 +0100

    reworking on the project

--- src/index.js
+++ src/index.js
@@ -1,23 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 // import { createStore } from 'redux';
-import { Provider } from 'react-redux';
-import  rootReducer  from './redux/Services/Reducers/rootReducer';
+import { Provider } from "react-redux";
+import rootReducer from "./redux/Services/Reducers/rootReducer";
 // const store = createStore(rootReducer)
-import store from './redux/store';
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render (
-    
-        <Provider store={store}><App /></Provider>
-      
-   
- 
+import store from "./redux/store";
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
